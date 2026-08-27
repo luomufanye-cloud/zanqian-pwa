1ef457a524bd9d3f8a2f72b07e349b61d15c44ec
luomufanye-cloud <luomufanye@gmail.com>
2026-08-27 18:01:25 +0800

    add sw.js

---
+++ sw.js
@@ -0,0 +1,42 @@
+/* 攒钱小台 Service Worker：缓存应用外壳，离线也能打开 */
+const CACHE = 'zqxt-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+  './apple-touch-icon.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(ASSETS).catch(() => {})).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((ks) => Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(e.request)
+        .then((resp) => {
+          try {
+            const copy = resp.clone();
+            caches.open(CACHE).then((c) => c.put(e.request, copy));
+          } catch (_) {}
+          return resp;
+        })
+        .catch(() => caches.match('./index.html'));
+    })
+  );
+});
